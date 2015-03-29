17350c4bc724f02bef05275142211dbfd363f21c
Victor Accarini <victor.accarini@gmail.com>
2015-03-28 20:26:55 -0400

    Initializing...

---
+++ Source/functions.c
@@ -0,0 +1,21 @@
+/*
+ * functions.c
+ *
+ * Created: 2/11/2015 2:07:19 PM
+ *  Author: Victor Accarini
+ */ 
+
+#include "functions.h"
+
+void F(unsigned char *arg1, unsigned char *arg2, unsigned char *arg3)
+{
+	putLine(arg1);
+	putLine(arg2);
+	putLine(arg3);
+}
+
+void D(unsigned char *arg1, unsigned char *arg2)
+{
+	putLine(arg1);
+	putLine(arg2);
+}
